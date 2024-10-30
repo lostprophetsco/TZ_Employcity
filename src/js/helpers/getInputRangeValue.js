8b2968e3becfa9412adac101c4d13dbb9a471eee
Vadim <lostprophetsco@gmail.com>
2024-10-30 14:27:57 +0300

    fix: add getRange JSDoc

--- src/js/helpers/getInputRangeValue.js
+++ src/js/helpers/getInputRangeValue.js
@@ -1,5 +1,18 @@
 import throttle from './throttle.js';
 
+/**
+ * @function getInputRangeValue
+ * @description Returns a function that will be called when range input changes.
+ *              The returned function will update the given element with the
+ *              current value of the range input.
+ * @param {string} elementToGet CSS selector of the range input element.
+ * @param {string} elementToSet CSS selector of the element to set the value to.
+ * @param {number} delay The minimum time (in milliseconds) to wait between
+ *                       invocations of the returned function. If the returned
+ *                       function is called again before the given delay has
+ *                       passed, the returned function will not be called until
+ *                       the given delay has passed.
+ */
 const getInputRangeValue = (elementToGet, elementToSet, delay = 0) => {
   const inputRange = document.querySelector(elementToGet);
   const rangeValue = document.querySelector(elementToSet);
